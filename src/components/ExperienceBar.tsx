997e0c2eb620e74548404e1feaad4665f822127c
Melqui <43079783+melquisedeque-magalhaes@users.noreply.github.com>
2021-02-23 02:11:07 -0300

    firts commit and create experience bar

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,16 @@
+export default function ExperienceBar() {
+    return (
+        <header className="experience-bar">
+            <span>0 xp</span>
+            <div>
+                <div style={{ width: '50%' }} />
+                <span 
+                    style={{ left: '50%'}}
+                    className='current-experience'> 
+                    300 xp 
+                </span>
+            </div>
+            <span>600 xp</span>
+        </header>
+    )
+}
